1e6b32632460cc0ff919d731d14454d662075ee3
southben <723158053@qq.com>
2019-05-22 17:58:42 +0800

    测试

---
+++ other/src/mycat/SplitStringHash.java
@@ -0,0 +1,20 @@
+package mycat;
+
+import io.mycat.route.function.PartitionByString;
+
+public class SplitStringHash {
+
+	public static void main(String[] args) {
+	      PartitionByString rule = new PartitionByString();
+	      String idVal=null;
+	      rule.setPartitionLength("512");
+	      rule.setPartitionCount("2");
+	      rule.init();
+	      rule.setHashSlice("0:2");
+	    idVal = "0";
+	    System.out.println(0 == rule.calculate(idVal));
+//	    idVal = "45a";
+//	    Assert.assertEquals(true, 1 == rule.calculate(idVal));
+
+	}
+}
